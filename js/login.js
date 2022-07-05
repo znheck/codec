185e0f84abf56fb4b1e3ffea196dcd11b5932318
znheck <101906205+znheck@users.noreply.github.com>
2022-07-05 19:13:02 -0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,11 @@
+function login() {
+    window.location.href = 'dashboard.html';
+}
+
+function register() {
+    window.location.href = 'register.html'
+}
+
+function recoverpassowrd() {
+    window.location.href = 'passowrd.html'
+}
